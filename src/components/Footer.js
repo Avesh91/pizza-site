66fcf5659616b4f851b018c183911b80960eda0e
Avesh91 <109279836+Avesh91@users.noreply.github.com>
2022-10-05 02:44:31 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,47 +5,106 @@ const Footer = () => {
         <div>
 
 
-            <section className="footer">
-            <div className='f-data'>
-                <div className="box-container">
-                    <div className="box">
-                        <h3>locations</h3>
-                        <a >india</a>
-                        <a >japan</a>
-                        <a >russia</a>
-                        <a >USA</a>
-                        <a >france</a>
-                    </div>
-                    <div className="box">
-                        <h3>quick links</h3>
-                        <a >home</a>
-                        <a >dishes</a>
-                        <a >about</a>
-                        <a >menu</a>
-                        <a >reivew</a>
-                        <a >order</a>
-                    </div>
-                    <div className="box">
-                        <h3>contact info</h3>
-                        <a >+123-456-7890</a>
-                        <a >+111-222-3333</a>
-                        <a >aveshansari7474@gmail.com</a>
-                        <a >aveshansari7474@gmail.com</a>
-                        <a >indore, india - 400104</a>
-                    </div>
-                    <div className="box">
-                        <h3>follow us</h3>
-                        <a >facebook</a>
-                        <a >twitter</a>
-                        <a >instagram</a>
-                        <a >linkedin</a>
-                    </div>
-                </div>
-                </div>
-                <div className="credit" > copyright @ 2022 by <span>mr. Avesh</span> </div>
+//             <section className="footer">
+//             <div className='f-data'>
+//                 <div className="box-container">
+//                     <div className="box">
+//                         <h3>locations</h3>
+//                         <a >india</a>
+//                         <a >japan</a>
+//                         <a >russia</a>
+//                         <a >USA</a>
+//                         <a >france</a>
+//                     </div>
+//                     <div className="box">
+//                         <h3>quick links</h3>
+//                         <a >home</a>
+//                         <a >dishes</a>
+//                         <a >about</a>
+//                         <a >menu</a>
+//                         <a >reivew</a>
+//                         <a >order</a>
+//                     </div>
+//                     <div className="box">
+//                         <h3>contact info</h3>
+//                         <a >+123-456-7890</a>
+//                         <a >+111-222-3333</a>
+//                         <a >aveshansari7474@gmail.com</a>
+//                         <a >aveshansari7474@gmail.com</a>
+//                         <a >indore, india - 400104</a>
+//                     </div>
+//                     <div className="box">
+//                         <h3>follow us</h3>
+//                         <a >facebook</a>
+//                         <a >twitter</a>
+//                         <a >instagram</a>
+//                         <a >linkedin</a>
+//                     </div>
+//                 </div>
+//                 </div>
+//                 <div className="credit" > copyright @ 2022 by <span>mr. Avesh</span> </div>
 
-            </section>
-
+//             </section>
+    <div>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />  
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" />  
+  <footer className="section bg-footer">  
+    <div className="container">  
+      <div className="row">  
+        <div className="col-lg-3">  
+          <div className>  
+            <h6 className="footer-heading text-uppercase text-white">Information</h6>  
+            <ul className="list-unstyled footer-link mt-4">  
+              <li><a href>Pages</a></li>  
+              <li><a href>Our Team</a></li>  
+              <li><a href>Feuchers</a></li>  
+              <li><a href>Pricing</a></li>  
+            </ul>  
+          </div>  
+        </div>  
+        <div className="col-lg-3">  
+          <div className>  
+            <h6 className="footer-heading text-uppercase text-white">Ressources</h6>  
+            <ul className="list-unstyled footer-link mt-4">  
+              <li><a href>Monitoring Grader </a></li>  
+              <li><a href>Video Tutorial</a></li>  
+              <li><a href>Term &amp; Service</a></li>  
+              <li><a href>Zeeko API</a></li>  
+            </ul>  
+          </div>  
+        </div>  
+        <div className="col-lg-2">  
+          <div className>  
+            <h6 className="footer-heading text-uppercase text-white">Help</h6>  
+            <ul className="list-unstyled footer-link mt-4">  
+              <li><a href>Sign Up </a></li>  
+              <li><a href>Login</a></li>  
+              <li><a href>Terms of Services</a></li>  
+              <li><a href>Privacy Policy</a></li>  
+            </ul>  
+          </div>  
+        </div>  
+        <div className="col-lg-4">  
+          <div className>  
+            <h6 className="footer-heading text-uppercase text-white">Contact Us</h6>  
+            <p className="contact-info mt-4">Contact us if need help withanything</p>  
+            <p className="contact-info">+01 123-456-7890</p>  
+            <div className="mt-5">  
+              <ul className="list-inline">  
+                <li className="list-inline-item"><a href="#"><i className="fab facebook footer-social-icon fa-facebook-f" /></a></li>  
+                <li className="list-inline-item"><a href="#"><i className="fab twitter footer-social-icon fa-twitter" /></a></li>  
+                <li className="list-inline-item"><a href="#"><i className="fab google footer-social-icon fa-google" /></a></li>  
+                <li className="list-inline-item"><a href="#"><i className="fab apple footer-social-icon fa-apple" /></a></li>  
+              </ul>  
+            </div>  
+          </div>  
+        </div>  
+      </div>  
+    </div>  
+    <div className="text-center mt-5">  
+      <p className="footer-alt mb-0 f-14">2022 © Avesh Ansari, All Rights Reserved </p>  
+    </div>  
+  </footer>
         </div>
     )
 }
